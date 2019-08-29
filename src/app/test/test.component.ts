e43d7832104669e51021070b535debe91a3eafcc
Nick Kozlov <nk@buttonwallet.com>
2019-08-30 02:40:22 +0300

    Add UI to test

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,54 +1,74 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {TrustProviderService} from '../trust-provider.service';
-import {Subscription} from 'rxjs';
-import {CosmosService, CosmosServiceInstance} from '../cosmos.service';
-import {HttpClient} from '@angular/common/http';
-import {CoinType} from '@trustwallet/types/lib/CoinType';
-import {switchMap} from 'rxjs/operators';
-import {CosmosAccount} from '@trustwallet/rpc/lib';
+import {Component, ElementRef, OnInit, ViewChild} from "@angular/core";
+import {TrustProviderService} from "../trust-provider.service";
+import {Subscription} from "rxjs";
+import {CosmosService, CosmosServiceInstance} from "../cosmos.service";
+import {HttpClient} from "@angular/common/http";
+import {CoinType} from "@trustwallet/types/lib/CoinType";
+import {switchMap} from "rxjs/operators";
+import {CosmosAccount} from "@trustwallet/rpc/lib";
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.scss'],
+  selector: "app-test",
+  templateUrl: "./test.component.html",
+  styleUrls: ["./test.component.scss"],
 })
 export class TestComponent {
-  subscription: Subscription;
-  cosmosInstance: CosmosServiceInstance;
-  account: string;
+  subscription : Subscription;
+  cosmosInstance : CosmosServiceInstance;
+  account : string;
+  showValue : string;
+  currentSum : string = "ATOM";
+  @ViewChild("input")
+  inputElement : ElementRef;
 
-  @ViewChild('input')
-  inputElement: ElementRef;
-
-  constructor(private trustProvider: TrustProviderService, private cosmos: CosmosService, private http: HttpClient) {
+  constructor( private trustProvider : TrustProviderService, private cosmos : CosmosService, private http : HttpClient ) {
     // TODO: fix at service level
     this.subscription = this.trustProvider.currentAccount$
-      .subscribe((account) => {
+      .subscribe(( account ) => {
         this.account = account;
         this.cosmosInstance = this.cosmos.getInstance(account);
       });
   }
 
+  calculateAmount() {
+    this.showValue =
+      this.currentSum == "ATOM" ?
+        "$" + (this.inputElement.nativeElement.value * 2.64).toFixed(2)
+        : this.inputElement.nativeElement.value;
+    // if(this.currentSum == 'ATOM') {
+    //   this.showValue = '$'+ (this.inputElement.nativeElement.value * 2.64).toFixed(2);
+    // } else if (this.currentSum == 'USD') {
+    //   this.showValue = ;
+
+  }
+
+  swap() {
+    this.currentSum = this.currentSum != "ATOM" ? "ATOM" : "USD";
+    this.calculateAmount();
+  }
+
   stake() {
+    // @ts-ignore
+    let amount = this.inputElement.nativeElement.value * 1000000;
     this.cosmosInstance.getAccountOnce$(this.account).pipe(
-      switchMap((account: CosmosAccount) => {
+      switchMap(( account : CosmosAccount ) => {
         // const {accountNumber, sequence} = account;
         // TODO: use validator address here
-        const addressTo = 'cosmosvaloper102ruvpv2srmunfffxavttxnhezln6fnc54at8c';
+        const addressTo = "cosmosvaloper102ruvpv2srmunfffxavttxnhezln6fnc54at8c";
         // TODO: take it from input
-        const amount = '1';
-        return this.trustProvider.signStake(CoinType.cosmos, addressTo, this.account, '1',
+        const amount = "1";
+        return this.trustProvider.signStake(CoinType.cosmos, addressTo, this.account, "1",
           account.sequence.toString(),
           account.accountNumber.toString(),
         );
       }),
-      switchMap((result) => {
+      switchMap(( result ) => {
         const fixedResult = result.substring(9, result.length - 2);
         return this.cosmosInstance.broadcastTx(fixedResult);
       })).subscribe(
-      (answer) => {
+      ( answer ) => {
         alert(JSON.stringify(answer));
-      }
+      },
     );
   }
 
