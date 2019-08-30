3401718ebf2f953e06f2d88549159c24b8ba859b
Nick Kozlov <nk@buttonwallet.com>
2019-08-30 03:44:41 +0300

    Add tests example

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,30 @@
+import {TestBed, async} from "@angular/core/testing";
+import {RouterTestingModule} from "@angular/router/testing";
+import {AppComponent} from "./app.component";
+import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
+import {HttpClientModule} from "@angular/common/http";
+
+describe("AppComponent", () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+      ],
+      declarations: [
+        AppComponent,
+      ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA,
+      ],
+    }).compileComponents();
+  }));
+
+
+  it(`should build app`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {MainComponent} from './main/main.component';
-import {HttpClientModule} from '@angular/common/http';
-import {CommonModule} from '@angular/common';
-import {TestComponent} from './test/test.component';
-import {AccountInfoComponent} from './account-info/account-info.component';
-import {AllDelegatorsComponent} from './all-delegators/all-delegators.component';
-import {DetailsComponent} from './details/details.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
+import {AppRoutingModule} from "./app-routing.module";
+import {AppComponent} from "./app.component";
+import {MainComponent} from "./main/main.component";
+import {HttpClientModule} from "@angular/common/http";
+import {CommonModule} from "@angular/common";
+import {TestComponent} from "./test/test.component";
+import {AccountInfoComponent} from "./account-info/account-info.component";
+import {AllDelegatorsComponent} from "./all-delegators/all-delegators.component";
+import {DetailsComponent} from "./details/details.component";
 
 @NgModule({
   declarations: [
@@ -17,16 +17,17 @@ import {DetailsComponent} from './details/details.component';
     MainComponent,
     AllDelegatorsComponent,
     DetailsComponent,
-    TestComponent
+    TestComponent,
   ],
   imports: [
     CommonModule,
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {
 }
