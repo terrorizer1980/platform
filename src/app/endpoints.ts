3947d94093d6a38f51bfe4c1b4546f42dc82d548
Viktor Radchenko <1641795+vikmeup@users.noreply.github.com>
2019-08-29 17:50:08 -0700

    Use https

--- src/app/endpoints.ts
+++ src/app/endpoints.ts
@@ -1,3 +1,3 @@
 export const priceUri = 'https://api.trustwallet.com/prices';
 export const cosmosEndpoint = 'https://cosmos-rpc.trustwalletapp.com';
-export const blockatlasEndpoint = 'http://blockatlas.trustwalletapp.com';
+export const blockatlasEndpoint = 'https://blockatlas.trustwalletapp.com';
