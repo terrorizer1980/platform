365807402287b2db9b2ec4984395427a1c1ad38d
Leone Parise <1442927+leoneparise@users.noreply.github.com>
2019-09-13 06:25:39 -0300

    Add sentry (#85)
    * Add sentry package
    
    * Integrate sentry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
 import "reflect-metadata";
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule, ErrorHandler, Injectable } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { RouterModule } from "@angular/router";
 import { CoreModule } from "./core/core.module";
 import { SharedModule } from "./shared/shared.module";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { ReactiveFormsModule } from "@angular/forms";
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: "https://2083a18ff4264ba2a86704ee8d8bc445@sentry.io/1678583"
+});
+
+@Injectable()
+export class SentryErrorHandler implements ErrorHandler {
+  constructor() {}
+  handleError(error) {
+    const eventId = Sentry.captureException(error.originalError || error);
+    Sentry.showReportDialog({ eventId });
+  }
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,7 +32,7 @@ import { ReactiveFormsModule } from "@angular/forms";
     BrowserModule,
     NgbModule
   ],
-  providers: [],
+  providers: [{ provide: ErrorHandler, useClass: SentryErrorHandler }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
